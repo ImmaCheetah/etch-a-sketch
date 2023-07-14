a4f17466c6d00627829b2b9a129b2968ee8b5874
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-07-14 11:49:09 -0400

    Create a div to hold the squares made by createCol

--- script.js
+++ script.js
@@ -6,12 +6,23 @@ let container = document.createElement('div');
 container.className = 'container';
 document.body.appendChild(container);
 
+let column = document.createElement('div');
+column.className = 'column';
+container.appendChild(column);
+
 createCol(16);
 
 function createCol(colNum) { 
     for (i = 0; i < colNum; i++) {
         let square = document.createElement('div');
-        container.appendChild(square).className = ('col square');
+        column.appendChild(square).className = ('col square');
     }
-}      
+    // let columns = document.querySelectorAll('.col square');
+    // document.column.appendChild(columns);   
+}   
+
+function createRow(rowNum) {
+
+}
+
 
