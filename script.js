c65492ac4b4f38f83e5822c6aba6bb85c93b7127
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-07-14 14:43:43 -0400

    Comment out creteGrid and createCol
    - Create function of createRow to use one loop to make grid with nesting

--- script.js
+++ script.js
@@ -7,24 +7,35 @@ container.className = 'container';
 document.body.appendChild(container);
 
 
-
-createCol(16);
-createCol(16);
-
-function createCol(colNum) { 
-    let column = document.createElement('div');
-    column.className = 'column';
-    container.appendChild(column);
-    for (i = 0; i < colNum; i++) {
-        let square = document.createElement('div');
-        column.appendChild(square).className = ('col square');
-    }
-    // let columns = document.querySelectorAll('.col square');
-    // document.column.appendChild(columns);   
-}   
+// function createCol(colNum) { 
+//     let column = document.createElement('div');
+//     column.className = 'column';
+//     container.appendChild(column);
+//     for (i = 0; i < colNum; i++) {
+//         let square = document.createElement('div');
+//         column.appendChild(square).className = ('col square');
+//     }
+// }   
 
 function createRow(rowNum) {
-
+    for (i = 0; i < rowNum; i++) {
+        let square = document.createElement('div');
+        container.appendChild(square).className = ('row square');
+    }
 }
+
+createRow(16);
+
+
+
+
+
+// function createGrid(rowNum, colNum) {
+//     for (i = 0; i < rowNum; i++) {
+//         createCol(colNum);
+//     }
+// }
+
+
 
 
