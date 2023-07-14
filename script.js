e473ea635105e2771a7814e1ba1905492fdc8432
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-07-14 11:39:08 -0400

    Change createGrid to just create col squares

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ let container = document.createElement('div');
 container.className = 'container';
 document.body.appendChild(container);
 
-createGrid(16, 16);
+createCol(16);
 
-function createGrid(colNum, rowNum) { 
-    for (i = 0; i < rowNum; i++) {
-        for (j = 0; j < colNum; j++) {
-            let square = document.createElement('div');
-            container.appendChild(square).className = ('col square');
-        }
-    }      
-}
+function createCol(colNum) { 
+    for (i = 0; i < colNum; i++) {
+        let square = document.createElement('div');
+        container.appendChild(square).className = ('col square');
+    }
+}      
 
