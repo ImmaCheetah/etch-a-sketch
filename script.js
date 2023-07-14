1b2336d0d10fba7e2d9bffc0e62a151fed3ba021
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-07-14 19:19:20 -0400

    Create arrow function with forEach to colour squares

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ container.className = 'container';
 document.body.appendChild(container);
 
 function createGrid(rowNum) {
-    for (i = 0; i < rowNum; i++) {
+    for (let i = 0; i < rowNum; i++) {
         let square = document.createElement('div');
         container.appendChild(square).className = ('row square');
-        for (j = 0; j < rowNum; j++) {
+        for (let j = 0; j < rowNum; j++) {
             let indivSquare = document.createElement('div');
             square.appendChild(indivSquare).className = ('individual square');
         }
@@ -16,13 +16,18 @@ function createGrid(rowNum) {
 
 createGrid(16);
 
-let individual = document.querySelector('.individual');
-console.log(individual);
-individual.addEventListener('mouseover', paint);
+let squares = document.querySelectorAll('.individual');
 
 function paint(e) {
-    individual.style.backgroundColor = 'black';
+    squares.style.backgroundColor = 'black';
 }
+
+squares.forEach(square => {
+    square.addEventListener('mouseover', () => square.style.backgroundColor = 'black');
+  });
+
+
+
 
 
 
