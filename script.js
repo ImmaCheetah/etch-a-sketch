029e1a35a8d87e9f07b109452124681426e9f8a8
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-07-17 16:35:57 -0400

    Add colour button to change colour when selected and css to everything

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+let buttonDiv = document.querySelector('.buttons')
+
 let gridSizeBtn = document.createElement('button');
 gridSizeBtn.textContent = "Grid Size";
-document.body.appendChild(gridSizeBtn);
+buttonDiv.appendChild(gridSizeBtn);
 
 let clearBtn = document.createElement('button');
 clearBtn.textContent = "Clear";
-document.body.appendChild(clearBtn);
+buttonDiv.appendChild(clearBtn);
 
 let colourBtn = document.createElement('input');
 colourBtn.setAttribute('type', 'color');
 colourBtn.setAttribute('value', '#000000');
-document.body.appendChild(colourBtn);
-
+buttonDiv.appendChild(colourBtn);
 
 // Create container to hold grid
 let container = document.createElement('div');
@@ -26,7 +27,7 @@ function createGrid(colNum) {
         
         for (let j = 0; j < colNum; j++) {
             let indivSquare = document.createElement('div');
-            indivSquare.style.cssText = `height: ${size}px; width: ${size}px;`;
+            indivSquare.style.cssText = `height: ${size-2}px; width: ${size-2}px;`;
             square.appendChild(indivSquare).className = ('individual square');
             // Add event listener here to avoid duplicates being created every time the loop runs
             indivSquare.addEventListener('mouseover', () => indivSquare.style.backgroundColor = 'black');
@@ -38,6 +39,9 @@ function createGrid(colNum) {
 createGrid(24);
 
 gridSizeBtn.addEventListener('click', promptUser);
+
+colourBtn.addEventListener('click', setColour)
+
 
 let squares = document.querySelectorAll('.individual');
 
